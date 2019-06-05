dd4a97621b35019a67a97127e84a432141acc1ea
samuelya <samuel@gerges.org>
2019-06-05 22:49:31 +0930

    update

--- InstallChocoPackages.ps1
+++ InstallChocoPackages.ps1
@@ -1,4 +1,7 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Enable-PSRemoting -Force -SkipNetworkProfileCheck
+Set-ExecutionPolicy Bypass -Scope Process -Force
+$sb = { iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')) }
+Invoke-Command -ScriptBlock $sb
 choco install -y notepadplusplus
 choco install -y googlechrome
 choco install -y sqltoolbelt "/FTP /products:'SQL Compare, SQL Data Compare'"
