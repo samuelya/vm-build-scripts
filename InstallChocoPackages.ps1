d4821a619e31d0fc02ec0cb6f2897ee44a3a7be9
samuelya <samuel@gerges.org>
2019-06-05 06:50:17 +0000

    inti

---
+++ InstallChocoPackages.ps1
@@ -0,0 +1,5 @@
+﻿Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco install notepadplusplus -y
+choco install googlechrome -y
+choco install sqltoolbelt "/FTP /products:'SQL Compare, SQL Data Compare'" -y
+choco install sql-server-2017 -y
