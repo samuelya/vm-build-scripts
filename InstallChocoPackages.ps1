fbd07242c874a25f69b006e3b5e0b53a6e3a9fd4
samuelya <samuel@gerges.org>
2019-06-05 17:07:06 +0930

    Update InstallChocoPackages.ps1
    restart computer after install

--- InstallChocoPackages.ps1
+++ InstallChocoPackages.ps1
@@ -1,6 +1,7 @@
-﻿Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install notepadplusplus -y
 choco install googlechrome -y
 choco install sqltoolbelt "/FTP /products:'SQL Compare, SQL Data Compare'" -y
 choco install sql-server-2017 -y
 choco install postman -y
+Restart-Computer
