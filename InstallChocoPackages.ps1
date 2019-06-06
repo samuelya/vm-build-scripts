86f2d27e3ae416df90666f8adc9b7221a2b649a8
samuelya <samuel@gerges.org>
2019-06-06 01:40:02 +0000

    update

--- InstallChocoPackages.ps1
+++ InstallChocoPackages.ps1
@@ -2,9 +2,9 @@ Enable-PSRemoting -Force -SkipNetworkProfileCheck
 Set-ExecutionPolicy Bypass -Scope Process -Force
 $sb = { iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')) }
 Invoke-Command -ScriptBlock $sb
-choco install -y notepadplusplus
-choco install -y googlechrome
-choco install -y sqltoolbelt "/FTP /products:'SQL Compare, SQL Data Compare'"
-choco install -y sql-server-2017
-choco install -y postman
+choco install -y notepadplusplus --force
+choco install -y googlechrome --force
+choco install -y sql-server-2017 --force
+choco install -y postman --force
+choco install -y sqltoolbelt "/products:'SQL Compare, SQL Data Compare'" --force
 Restart-Computer
